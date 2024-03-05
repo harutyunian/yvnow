de69ca8e7f813a53779583568f80e24e33ec8dec
Arthur Harutyunyan <hayart98@gmail.com>
2024-03-05 21:48:55 +0400

    setup some pages fix dependencies installing problem added toolkit

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
-import { StyleSheet } from "react-native";
+import React from "react";
 import { Provider } from "react-redux";
-import { store } from "./src/store";
+import store from "./src/store";
 import Route from "./src/pages/Route";
-import NavigationBar from "./src/components/NavigationBar/NavigationBar";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Route />
-      <NavigationBar />
+        <Route />
     </Provider>
   );
 }
