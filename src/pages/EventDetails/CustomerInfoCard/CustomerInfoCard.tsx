f9261c095181a8a3cb3d688baa8e3448f31226a3
Arthur Harutyunyan <hayart98@gmail.com>
2024-03-19 20:14:15 +0400

    added time on event details by date

--- src/pages/EventDetails/CustomerInfoCard/CustomerInfoCard.tsx
+++ src/pages/EventDetails/CustomerInfoCard/CustomerInfoCard.tsx
@@ -1,102 +1,91 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {View, Text, StyleSheet, Image} from "react-native";
 import dayjs from "dayjs";
-import { LocationIcon } from "../../../components/Svg/Svg";
-import { useAppSelector } from "../../../hook/reduxHooks";
+import {useAppSelector} from "../../../hook/reduxHooks";
 
 export default function CustomerInfoCard() {
-  const colors = useAppSelector((state) => state.theme);
-  const eventDetails = useAppSelector((state) => state.eventDetails);
-  const {
-    title,
-    startDate,
-    user: { address },
-  } = eventDetails;
+    const colors = useAppSelector((state) => state.theme);
+    const eventDetails = useAppSelector((state) => state.eventDetails);
+    const {
+        startDate,
+        endDate
+    } = eventDetails;
 
-  return (
-    <View style={[customerInfoCardStyle.container]}>
-      <View
-        style={[
-          customerInfoCardStyle.main,
-          { backgroundColor: colors.PRIMARY.MAIN },
-        ]}
-      >
-        <View style={[customerInfoCardStyle.titleContainer]}>
-          <Text style={[customerInfoCardStyle.title]}>{title}</Text>
-          <View style={[customerInfoCardStyle.dateContainer]}>
-            <Text
-              style={[
-                customerInfoCardStyle.date,
-                { color: colors.PRIMARY.MAIN },
-              ]}
+    return (
+        <View style={[customerInfoCardStyle.container]}>
+            <View
+                style={[
+                    customerInfoCardStyle.main,
+                    {backgroundColor: colors.ACCENT['6']},
+                ]}
             >
-              {dayjs(startDate).format("DD MMM YYYY")}
-            </Text>
-          </View>
+                <View style={[customerInfoCardStyle.iconContainer]}>
+                    <Image
+                        style={[customerInfoCardStyle.iconSizes]}
+                        source={require('../../../../assets/calendar.png')}
+                    />
+                    <View style={[customerInfoCardStyle.dateWrapper]}>
+                        <Text style={[customerInfoCardStyle.iconNameText]}>Date</Text>
+                        <Text style={[customerInfoCardStyle.date,{color: colors.ACCENT['1']}]}>{dayjs(startDate).format('DD-MM-YYYY')}</Text>
+                    </View>
+                </View>
+                <View style={[customerInfoCardStyle.iconContainer]}>
+                    <Image
+                        style={[customerInfoCardStyle.iconSizes]}
+                        source={require('./../../../../assets/clock.png')}
+                    />
+                    <View style={[customerInfoCardStyle.dateWrapper]}>
+                        <Text style={[customerInfoCardStyle.iconNameText]}>Time</Text>
+                        <Text style={[customerInfoCardStyle.date,{color: colors.ACCENT['1']}]}>
+                            {dayjs(startDate).format('hh:ss')}-
+                            {dayjs(endDate).format('hh:ss')}
+                        </Text>
+                    </View>
+                </View>
+            </View>
         </View>
-        <View style={[customerInfoCardStyle.address]}>
-          <LocationIcon style={[customerInfoCardStyle.locationIcon]} />
-          <Text style={[customerInfoCardStyle.addressTitle]}>{address}</Text>
-        </View>
-      </View>
-    </View>
-  );
+    );
 }
 
 const customerInfoCardStyle = StyleSheet.create({
-  main: {
-    width: "90%",
-    height: 90,
-    borderRadius: 15,
-    paddingTop: 11,
-    paddingRight: 15,
-    paddingBottom: 10,
-    paddingLeft: 20,
-    display: "flex",
-    justifyContent: "space-between",
-  },
-  address: {
-    display: "flex",
-    flexDirection: "row",
-    columnGap: 10,
-  },
-  addressTitle: {
-    fontWeight: "400",
-    fontSize: 14,
-    color: "white",
-  },
-  locationIcon: {
-    width: 12,
-    height: 16,
-  },
-  titleContainer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  dateContainer: {
-    width: 81,
-    height: 24,
-    backgroundColor: "white",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 12,
-  },
-  date: {
-    fontWeight: "500",
-    fontSize: 10,
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: "700",
-    color: "white",
-  },
-  container: {
-    bottom: 30,
+    main: {
+        width: "80%",
+        height: 80,
+        borderRadius: 15,
+        paddingTop: 11,
+        paddingRight: 15,
+        paddingBottom: 10,
+        paddingLeft: 20,
+        display: "flex",
+        flexDirection: 'row',
+        justifyContent: "space-around",
+        alignItems: 'center'
+    },
+    iconSizes: {
+        width: 45,
+        height: 45
+    },
+    dateWrapper:{
 
-    width: "100%",
-    alignItems: "center",
-    display: "flex",
-  },
+    },
+    date:{
+        fontWeight: '500',
+        fontSize: 12
+    },
+    iconContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        columnGap: 12
+    },
+    iconNameText: {
+        color: '#64748B',
+        fontSize: 12,
+        fontWeight: '400'
+    },
+    container: {
+        bottom: 30,
+        width: "100%",
+        alignItems: "center",
+        display: "flex",
+    },
 });
