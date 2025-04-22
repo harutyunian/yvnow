731a17829a3649fcde84e58091c0a4ec0c140918
Arthur Harutyunyan <arharutyunyan@Arthurs-MacBook-Pro.local>
2025-04-22 15:50:22 +0400

    added new arch

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,15 @@
 module.exports = function (api) {
     api.cache(true);
+    const isProd = process.env.NODE_ENV === 'production';
+
     return {
         presets: ['module:metro-react-native-babel-preset'],
-        plugins: ['react-native-reanimated/plugin']
+        plugins: [
+            ['@babel/plugin-transform-class-properties', { loose: false }],
+            ['@babel/plugin-transform-private-methods', { loose: false }],
+            ['@babel/plugin-transform-private-property-in-object', { loose: false }],
+            isProd && 'babel-plugin-transform-remove-console',
+            'react-native-reanimated/plugin' // always last
+        ].filter(Boolean),
     };
 };
